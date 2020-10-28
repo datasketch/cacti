0261bfcd4a5a6451878f97bcacac1818dce8d1d2
David Daza <03dazal@gmail.com>
2020-10-28 07:41:59 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        'castleton-green': '#005a49',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
