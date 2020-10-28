e8e46c00c83accd25635ae4edd7154e8add61339
David Daza <03dazal@gmail.com>
2020-10-28 10:02:19 -0500

    add content to homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,12 @@ module.exports = {
         'castleton-green': '#005a49',
         'soft-white': '#fffdfd',
         'denim-blue': '#3841ae',
+        'outrageous-orange': '#fb6840',
+        'space-crayola': '#333f3d'
       },
+      fontFamily: {
+        neue: ['"neue-haas-grotesk-display"', 'sans-serif']
+      }
     },
   },
   variants: {},
